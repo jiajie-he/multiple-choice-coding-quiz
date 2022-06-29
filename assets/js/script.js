758ab27570b3b5cba19ccbedacb2c8d4e31a7e87
Jiajie <jiajiehe@yahoo.com>
2022-06-29 00:00:38 -1000

    basic layout

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+//TODO: start game, show a randomized question, trigger countdown timer
+//TODO: correct answer move on, incorrect answer minus 10 seconds from timer
+//TODO: store username and score to local storage
+//TODO: 
+//TODO: 
+//TODO: 
